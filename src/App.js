4cf63597e9eaacafef55c5837e3cc58ddeac7d92
unknown <rajan23024@gmail.com>
2023-05-18 16:30:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
+import { ThemeContext } from "./ThemeContext";
+
+import "./index.css";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isDarkMode ? "App dark" : "App"}>
+      <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+        <Header />
+        <Hero />
+        <About />
+        <CallToAction />
+        <Footer />
+      </ThemeContext.Provider>
     </div>
   );
 }
